8962db5011ae512a466e8a0ab88eb964551d2029
akshayk2022 <www.akshaykumar04855@gmail.com>
2023-06-02 23:12:36 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+    public void test(int a) {
+        int e;
+        e = a;
+        System.out.println(e);
+
+    }
+
+    public static void main(String[] args) {
+        Main b = new Main();
+        b.test(4);
+    }
+}
